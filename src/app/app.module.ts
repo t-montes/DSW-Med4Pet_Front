f82432de6438ee351a097fde24ae6c605b04a647
j.marinm <j.marinm@uniandes.edu.co>
2021-10-13 15:22:34 -0500

    Modulo vet v2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+
+import { VeterinarioModule } from './veterinario/veterinario.module'
+import { ContactoModule } from './contacto/contacto.module'
 
 @NgModule({
   declarations: [
@@ -10,7 +15,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    VeterinarioModule,
+    ContactoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
