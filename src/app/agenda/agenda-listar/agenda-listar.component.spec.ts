49eaa116a8b1e337a979a6339eea92503c247613
Tony Santiago Montes Buitrago <t.montes@uniandes.edu.co>
2021-10-17 15:01:03 -0500

    Agregar pruebas view

--- src/app/agenda/agenda-listar/agenda-listar.component.spec.ts
+++ src/app/agenda/agenda-listar/agenda-listar.component.spec.ts
@@ -5,26 +5,70 @@ import { DebugElement } from '@angular/core';
 
 import { AgendaListarComponent } from './agenda-listar.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import faker from 'faker';
+import { Cita } from 'src/app/cita/cita';
+import { Agenda } from '../agenda';
 
 describe('AgendaListarComponent', () => {
   let component: AgendaListarComponent;
   let fixture: ComponentFixture<AgendaListarComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ AgendaListarComponent ],
       imports: [HttpClientTestingModule]
-    })
-    .compileComponents();
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(AgendaListarComponent);
     component = fixture.componentInstance;
+    /*
+    let paciente = new Cliente(
+      faker.random.number(),
+    );
+    let medioDePago = new MedioDePago(
+      faker.random.number(),
+    );
+    let serviciosTomados = [new Servicio(
+      faker.random.number(),
+    )];
+    */
+
+    let citas = [new Cita(
+      faker.random.number(),
+      faker.date.past(),
+      faker.lorem.sentence(),
+      faker.random.number(),
+      faker.random.boolean(),
+      faker.random.number(),
+      //paciente,
+      //medioDePago,
+      //serviciosTomados
+    )];
+
+    component.agendas = [
+      new Agenda(
+        faker.random.number(),
+        faker.random.number(),
+        faker.random.number(),
+        faker.random.number(),
+        citas
+      )
+    ]
+
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it("Should have a card header element ", ()=>{
+    expect(debug.query(By.css(".card-header")).nativeElement.innerText).toContain(
+      component.agendas[0].id
+    );
   });
 });
--- src/app/cita/cita-listar/cita-listar.component.spec.ts
+++ src/app/cita/cita-listar/cita-listar.component.spec.ts
@@ -5,26 +5,65 @@ import { DebugElement } from '@angular/core';
 
 import { CitaListarComponent } from './cita-listar.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import faker from 'faker';
+import { Agenda } from 'src/app/agenda/agenda';
+//import { Cliente } from 'src/app/cliente/cliente';
+//import { MedioDePago } from 'src/app/medioDePago/medioDePago';
+//import { Servicio } from 'src/app/servicio/servicio';
+import { Cita } from '../cita';
 
 describe('CitaListarComponent', () => {
   let component: CitaListarComponent;
   let fixture: ComponentFixture<CitaListarComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ CitaListarComponent ],
       imports: [HttpClientTestingModule]
-    })
-    .compileComponents();
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CitaListarComponent);
     component = fixture.componentInstance;
+    /*
+    let paciente = new Cliente(
+      faker.random.number(),
+    );
+    let medioDePago = new MedioDePago(
+      faker.random.number(),
+    );
+    let serviciosTomados = [new Servicio(
+      faker.random.number(),
+    )];
+    */
+
+    component.citas = [
+      new Cita(
+        faker.random.number(),
+        faker.date.past(),
+        faker.lorem.sentence(),
+        faker.random.number(),
+        faker.random.boolean(),
+        faker.random.number(),
+        //paciente,
+        //medioDePago,
+        //serviciosTomados
+      )
+    ]
+
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it("Should have a card header element", ()=>{
+    expect(debug.query(By.css(".card-header")).nativeElement.innerText).toContain(
+      component.citas[0].id
+    );
   });
 });
