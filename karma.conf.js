ec935f6bcdfab8f7e563ea6e42f05075bb00aee9
j.marinm <j.marinm@uniandes.edu.co>
2021-11-10 16:19:23 -0500

    Ya ahora si

--- karma.conf.js
+++ karma.conf.js
@@ -34,7 +34,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'],
+    browsers: ['ChromeHeadlessNoSandbox'],
     singleRun: true,
     restartOnFileChange: true,
     customLaunchers: {
