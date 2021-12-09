a227adb0dd50cae35f0346bb8fd9feebde36c7f3
Luccas Rojas <l.rojasb@uniandes.edu.co>
2021-12-08 23:52:51 -0500

    fix contacto formulario

--- src/app/cliente/cliente.module.ts
+++ src/app/cliente/cliente.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { ClienteListComponent } from './cliente-list/cliente-list.component';
 import { ClienteCreateComponent } from './cliente-create/cliente-create.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import {ContactoModule} from '../contacto/contacto.module'
 
 @NgModule({
   imports: [
     CommonModule,
-    ReactiveFormsModule
-
+    ReactiveFormsModule,
+    ContactoModule
   ],
   declarations: [ClienteListComponent, ClienteCreateComponent],
   exports: [ClienteListComponent, ClienteCreateComponent],
