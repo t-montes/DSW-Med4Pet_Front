99d6f1abe45d2ae69afec3c4a241064d9b0b85bc
Daniel Villar <d.villarg@uniandes.edu.co>
2021-12-08 23:56:11 -0500

    C2

--- src/app/cliente/cliente.module.ts
+++ src/app/cliente/cliente.module.ts
@@ -4,13 +4,13 @@ import { ClienteListComponent } from './cliente-list/cliente-list.component';
 import { ClienteCreateComponent } from './cliente-create/cliente-create.component';
 import { ClienteDetailComponent } from './cliente-detail/cliente-detail.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MascotaModule } from '../mascota/mascota.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    ReactiveFormsModule
-
-
+    ReactiveFormsModule,
+    MascotaModule
   ],
   declarations: [ClienteListComponent, ClienteCreateComponent, ClienteDetailComponent],
   exports: [ClienteListComponent, ClienteCreateComponent],
